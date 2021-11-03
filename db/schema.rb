9ed6cad9a740266aa1d140ccddcf926d53031dc6
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-11-02 19:13:11 -0500

    added Recipes table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_184508) do
+ActiveRecord::Schema.define(version: 2021_11_03_000156) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.integer "serving_size"
+    t.string "image"
+    t.string "directions"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_11_02_184508) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "recipes", "users"
 end
