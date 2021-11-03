761340e3d01d8e398638378a01a1a3ce70e6f654
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-11-03 11:45:17 -0500

    all tables are seeded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_03_001853) do
+ActiveRecord::Schema.define(version: 2021_11_03_164031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_11_03_001853) do
     t.string "food_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "recipe_id", null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -40,5 +42,6 @@ ActiveRecord::Schema.define(version: 2021_11_03_001853) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "ingredients", "recipes"
   add_foreign_key "recipes", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Ingredient.destroy_all
+Recipe.destroy_all
 User.destroy_all
 
-User.create(username: 'breck1', email: 'breck1@email.com', password: '123456')
+@admin = User.create!(username: 'breck1', email: 'breck1@email.com', password: '123456')
+
+puts "#{User.count} users created"
+
+@pbj = Recipe.create!(title: 'pbj', serving_size: 8, image: 'image.png', directions: 'Lorem Ipsum', user: @admin)
+
+puts "#{Recipe.count} recipes created"
+
+@basil = Ingredient.create!(food_name: 'pbj', recipe: @pbj)
+
+puts "#{Ingredient.count} ingredients created"
