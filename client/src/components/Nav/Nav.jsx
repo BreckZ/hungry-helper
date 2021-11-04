15c2ebe794fd428e197366a34cbf13a62346c5af
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-11-04 12:46:43 -0500

    updated lock file for deployment

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,32 +1,29 @@
 import "./Nav.css";
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 function Nav(props) {
   const { currentUser, handleSignOut } = props;
   return (
     <nav>
-      <NavLink className="logo" to="/">
+      <NavLink className='logo' to='/'>
         HUNGRY HELPER
       </NavLink>
       {currentUser ? (
-        <div className="links">
-          <span>HELLO, {currentUser.username}</span>
-          <NavLink className="link" to="/">HOME</NavLink>
-          <NavLink className="link" to="/recipes">RECIPES</NavLink>
-          <NavLink className='link' to='recipes/create'>CREATE RECIPE</NavLink>
+        <div className='links'>
+          <span className='link'>HELLO, {currentUser.username}</span>
+          <NavLink className='link' to='/'>HOME</NavLink>
+          <NavLink className='link' to='/recipes'>RECIPES</NavLink>
+          <NavLink className='link' to='/create'>CREATE</NavLink>
           <NavLink onClick={handleSignOut} className='link' to='/signout'>SIGN OUT</NavLink>
         </div>
       ) : (
           <div className="links">
-          <NavLink className="link" to="/">HOME</NavLink>
-          <NavLink className="link" to="/recipes">RECIPES</NavLink>
-          <NavLink to="/signin">SIGN IN</NavLink>
-          <NavLink className="link" to="/signup">SIGN UP</NavLink>
+          <NavLink className='link' to='/'>HOME</NavLink>
+          <NavLink className='link' to='/recipes'>RECIPES</NavLink>
+          <NavLink className='link'to='/signin'>SIGN IN</NavLink>
+          <NavLink className='link' to='/signup'>SIGN UP</NavLink>
         </div>
       )}
-      <div className="links">
-
-      </div>
     </nav>
   );
 }
---
+++ client/src/screens/RecipeCreate/RecipeCreate.jsx
@@ -0,0 +1,13 @@
+import './RecipeCreate.css'
+
+// import RecipeForm from './components/RecipeForm/RecipeForm.jsx'
+
+function RecipeCreate() {
+  return (
+    <div>
+      Create A Recipe
+    </div>
+  )
+}
+
+export default RecipeCreate
