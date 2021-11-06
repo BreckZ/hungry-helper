42d0092aaf7b5b1e9e82a68c433cde0100863d54
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-11-06 14:52:09 -0500

    added branding headers and content

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -28,11 +28,20 @@ function Home() {
         </div>
 
         <div className="sub-container-2">
-          <div className="sub-header-text">SUB HEADER 1</div>
-          <div className="sub-header-text">SUB HEADER 2</div>
-          <div className="sub-header-text">SUB HEADER 2</div>
+          <div className="sub-header-text">
+            <span className="branding-header branding">STORE</span>
+            <span className="branding-content branding">Finally a place that lets you keep all your recipes in a single spot!  The Hungry Helper keeps track of recipes for you and makes it easy to find what you want to cook up.</span>
+          </div>
+          <div className="sub-header-text">
+          <span className="branding-header branding">PLAN</span>
+            <span className="branding-content branding">Get some great ideas about your next meal!  User generated content will provide you with new recipes to try, while still having access to the reliable ones your family loves.</span>
+          </div>
+          <div className="sub-header-text">
+          <span className="branding-header branding">SAVOR</span>
+            <span className="branding-content branding">Something that is catchy and describes what is going on should be written here.  It will be helpful to the user to understand more about your really cool application</span>
+          </div>
         </div>
-        
+
         <div className="recipe-card">CARD 1</div>
         <div className="recipe-card">CARD 2</div>
         <div className="recipe-card">CARD 3</div>
