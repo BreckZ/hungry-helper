dfa81b83e34bd124e056ff3f71dfef1777d913f1
Breck Zeyer <breckzeyer@Brecks-Air.attlocal.net>
2021-11-06 14:33:07 -0500

    updated home page to have hero image and hero text

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,23 +1,38 @@
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <>
       <div className="home-container">
         <div className="sub-container-1">
-          <div className="hero-image">HERO IMAGE</div>
+          <div className="image-container">
+            <img
+              className="hero-image"
+              src="https://i.imgur.com/fJFwKlL.jpg"
+              alt="spaghetti and meatballs"
+            />
+          </div>
           <div className="subcon-1">
             <div className="hero-text-container">
               <span className="hero-text">DINNER </span>
-              <span className="hero-text">DESIGNED </span>
+              <span className="hero-text designed">DESIGNED </span>
               <span className="hero-text">ON YOUR TERMS</span>
             </div>
-            <button className="view-recipes-button">VIEW RECIPES</button>
+            <Link to="/recipes">
+              <button type="button" className="view-recipes-button">
+                VIEW RECIPES
+              </button>
+            </Link>
           </div>
         </div>
-        <div className="sub-header-text">SUB HEADER 1</div>
-        <div className="sub-header-text">SUB HEADER 2</div>
-        <div className="sub-header-text">SUB HEADER 2</div>
+
+        <div className="sub-container-2">
+          <div className="sub-header-text">SUB HEADER 1</div>
+          <div className="sub-header-text">SUB HEADER 2</div>
+          <div className="sub-header-text">SUB HEADER 2</div>
+        </div>
+        
         <div className="recipe-card">CARD 1</div>
         <div className="recipe-card">CARD 2</div>
         <div className="recipe-card">CARD 3</div>
